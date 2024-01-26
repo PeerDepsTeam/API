0a685e78a44e496bd4daac6cf16addc92f91ed33
miharyjoel <miharyjoel@gmail.com>
2024-01-26 19:57:09 +0300

    chore: init project

---
+++ src/main/java/com/peerdeps/peerdepsapi/PeerdepsapiApplication.java
@@ -0,0 +1,13 @@
+package com.peerdeps.peerdepsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerdepsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeerdepsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peerdeps/peerdepsapi/PeerdepsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peerdeps.peerdepsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerdepsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
