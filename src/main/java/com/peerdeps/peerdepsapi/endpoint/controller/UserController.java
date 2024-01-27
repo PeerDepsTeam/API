55aaf97d5f1c60d87491f901371b16dbd1366fcc
NyH1409 <hei.nyhasina@gmail.com>
2024-01-27 08:41:43 +0300

    fix: allow all origins

--- src/main/java/com/peerdeps/peerdepsapi/endpoint/controller/UserController.java
+++ src/main/java/com/peerdeps/peerdepsapi/endpoint/controller/UserController.java
@@ -4,6 +4,7 @@ import com.peerdeps.peerdepsapi.model.User;
 import com.peerdeps.peerdepsapi.service.UserService;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
+@CrossOrigin("*")
 public class UserController {
   private final UserService service;
 
