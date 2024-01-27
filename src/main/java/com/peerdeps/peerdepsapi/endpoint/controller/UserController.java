2dad445b19702e08584696f5054dbff5ffa95143
NyH1409 <hei.nyhasina@gmail.com>
2024-01-27 04:03:39 +0300

    feat: find all users

--- src/main/java/com/peerdeps/peerdepsapi/endpoint/controller/UserController.java
+++ src/main/java/com/peerdeps/peerdepsapi/endpoint/controller/UserController.java
@@ -2,9 +2,11 @@ package com.peerdeps.peerdepsapi.endpoint.controller;
 
 import com.peerdeps.peerdepsapi.model.User;
 import com.peerdeps.peerdepsapi.service.UserService;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,6 +17,14 @@ public class UserController {
   @GetMapping(value = "/users/{id}")
   public User getUser(@PathVariable("id") String id){
     return service.findById(id);
+  }
+
+  @GetMapping(value = "/users")
+  public List<User> findAll(
+      @RequestParam(value = "page", required = false) Integer page,
+      @RequestParam(value = "pageSize", required = false) Integer pageSize
+  ){
+    return service.findAll(page, pageSize);
   }
 
 }
--- src/main/java/com/peerdeps/peerdepsapi/service/UserService.java
+++ src/main/java/com/peerdeps/peerdepsapi/service/UserService.java
@@ -3,7 +3,10 @@ package com.peerdeps.peerdepsapi.service;
 import com.peerdeps.peerdepsapi.model.exception.NotFoundException;
 import com.peerdeps.peerdepsapi.repository.UserRepository;
 import com.peerdeps.peerdepsapi.model.User;
+import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -19,4 +22,12 @@ public class UserService {
   public User getUserByFirebaseIdAndEmail(String uid, String email) {
     return null;
   }
+
+  public List<User> findAll(Integer page, Integer pageSize){
+    int pageValue = page == null ? 0 : page - 1;
+    int sizeValue = pageSize == null ? 10 : pageSize;
+    Pageable pageable = PageRequest.of(pageValue, sizeValue);
+    return repository.findAll(pageable).stream().toList();
+  }
+
 }
