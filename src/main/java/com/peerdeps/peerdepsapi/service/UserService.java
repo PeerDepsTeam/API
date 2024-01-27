eedc1c84ce30dc4ff29eeab6f8ddf676e0ef7f02
NyH1409 <hei.nyhasina@gmail.com>
2024-01-27 06:45:10 +0300

    chore: sort user by balance descendante

--- src/main/java/com/peerdeps/peerdepsapi/service/UserService.java
+++ src/main/java/com/peerdeps/peerdepsapi/service/UserService.java
@@ -4,6 +4,7 @@ import com.peerdeps.peerdepsapi.model.exception.NotFoundException;
 import com.peerdeps.peerdepsapi.repository.UserRepository;
 import com.peerdeps.peerdepsapi.model.User;
 import com.peerdeps.peerdepsapi.service.calculator.BalanceCalculator;
+import java.util.Comparator;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.PageRequest;
@@ -30,7 +31,15 @@ public class UserService {
     int pageValue = page == null ? 0 : page - 1;
     int sizeValue = pageSize == null ? 10 : pageSize;
     Pageable pageable = PageRequest.of(pageValue, sizeValue);
-    return repository.findAll(pageable).stream().toList();
+    return repository.findAll(pageable).stream().toList().stream()
+        .map(balanceCalculator::computeBalance)
+        .sorted(Comparator.comparingDouble(user -> getCurrentCapital((User) user))
+            .reversed())
+        .toList();
+  }
+
+  private double getCurrentCapital(User user){
+    return user.getBudget().getCurrentCapital();
   }
 
 }
